636dccdc08cc8447512f6f81c98802c24b3b774a
Benjamin Marwell <bmarwell@apache.org>
2022-06-06 23:05:57 +0200

    [OUTPUT] add transitive deps; Lock output emitting

--- bundle/src/main/java/io/github/bmarwell/liberty/cowsay/Activator.java
+++ bundle/src/main/java/io/github/bmarwell/liberty/cowsay/Activator.java
@@ -1,8 +1,10 @@
 package io.github.bmarwell.liberty.cowsay;
 
 import com.github.ricksbrown.cowsay.plugin.CowExecutor;
-import java.util.Enumeration;
+import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.osgi.framework.BundleActivator;
@@ -11,13 +13,13 @@ import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
 
-import java.util.Dictionary;
-
 public class Activator implements BundleActivator, ManagedService {
 
   private static final Logger LOGGER = Logger.getLogger(Activator.class.getName());
 
   private ServiceRegistration<ManagedService> configRef;
+
+  private final Lock lock = new ReentrantLock();
 
   private String startmessage;
 
@@ -50,8 +52,6 @@ public class Activator implements BundleActivator, ManagedService {
     if (startmessage instanceof String) {
       this.startmessage = (String) startmessage;
     }
-
-    emitStartMessage();
   }
 
   private void emitStartMessage()  {
@@ -61,8 +61,13 @@ public class Activator implements BundleActivator, ManagedService {
       cowExecutor.setMessage(this.startmessage);
       String cowsay = cowExecutor.execute();
 
-      for (String cowsayline : cowsay.split("\n")) {
-        LOGGER.log(Level.SEVERE, cowsayline);
+      try {
+        lock.lock();
+        for (String cowsayline : cowsay.split("\n")) {
+          LOGGER.log(Level.SEVERE, cowsayline);
+        }
+      } finally {
+          lock.unlock();
       }
     }
   }
